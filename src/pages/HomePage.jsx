514f8fbe272077a9afd0699171a903ded4b514f4
MrVihks <viniciushkds@gmail.com>
2023-11-13 21:12:04 -0300

    :lipstick: feat: Mudando estilização

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,13 +6,11 @@ import ProgrammingIMG from "../assets/programming.svg"
 const HomePage = () => {
     return (
         <section className='home'>
+             <img src={ProgrammingIMG} alt="" height={300} width={300} />
             <article className='home-header'>
-            <img src={ProgrammingIMG} alt="" height={300} width={300} />
-        
-                <p>Seja bem-vindo ao meu portifólio meu nome é </p>
-                <h1><span>Vinicius Henrique</span>, e sou Desenvolvedor Web</h1>
-        
-              
+                <p>Seja bem-vindo ao meu portifólio, meu nome é </p>
+                <h1><span>Vinicius Henrique</span>, sou Desenvolvedor Web.</h1>
+                <button> <a href='https://github.com/MrVihks' target='__blank'>Github</a></button>
             </article>
 
         </section>
