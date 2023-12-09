cdffe6bfe352c8096b6ff81c58fed6f5ee52143d
MrVihks <viniciushkds@gmail.com>
2023-12-09 10:22:41 -0300

    :bug: feat: Arrumando alguns bugs

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
             <h1>Sobre mim</h1>
             <article className='about-content'>
                 <img src={ViniIMG} alt="" height={200} width={200}/>
-                <p>Olá, sou Vinicius Henrique, um jovem desenvolvedor web de 15 anos. Minha jornada no mundo da programação está apenas começando, a cada projeto, vejo uma oportunidade de crescimento e melhoria.</p>
+                <p>Olá, sou Vinicius Henrique, um jovem desenvolvedor web de 16 anos. Minha jornada no mundo da programação está apenas começando, a cada projeto, vejo uma oportunidade de crescimento e melhoria.</p>
             </article>
         </section>
     )
